180f9817b079ec7c8141a2e03203e0b35cf7b7d3
stege637 <stege637@student.otago.ac.nz>
2022-08-04 15:00:44 +1200

    added comments and added some changes

--- culturle/app/src/main/java/com/example/culturle/MainActivity.kt
+++ culturle/app/src/main/java/com/example/culturle/MainActivity.kt
@@ -10,8 +10,12 @@ import androidx.appcompat.app.AppCompatActivity
 open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle ? ) {
         super.onCreate(savedInstanceState)
+        //Set the screen to the activity_main.xml
         setContentView(R.layout.activity_main)
+        //Set button object to the practiceButton id
         val practiceButtonObj: Button = findViewById<View>(R.id.practiceButton) as Button
+        //Create button listen event to switch over to Practice class activity
+        // Intent is the method of handling the switch
         practiceButtonObj.setOnClickListener {
             val intent = Intent(this, Practice::class.java)
             startActivity(intent)
--- culturle/app/src/main/java/com/example/culturle/Practice.kt
+++ culturle/app/src/main/java/com/example/culturle/Practice.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class Practice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Load practice.xml
         setContentView(R.layout.practice)
     }
 }
