1a01e649e66e3f066ca2f092447a7099e64e7bcf
stege637 <stege637@student.otago.ac.nz>
2022-08-04 14:10:03 +1200

    Made some changes

--- culturle/app/src/main/java/com/example/culturle/MainActivity.kt
+++ culturle/app/src/main/java/com/example/culturle/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.culturle
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+open class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle ? ) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val practiceButtonObj: Button = findViewById<View>(R.id.practiceButton) as Button
+        practiceButtonObj.setOnClickListener {
+            val intent = Intent(this, Practice::class.java)
+            startActivity(intent)
+        }
     }
 }
