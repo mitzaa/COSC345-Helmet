fc2cdc4d9deddf4e5faec5d9a9e67edfd8355819
Michael Young <michael.c.young00@gmail.com>
2022-08-11 09:53:22 +1200

    Added artscreen

---
+++ culturle/app/src/main/java/com/example/culturle/Artwork.kt
@@ -0,0 +1,11 @@
+package com.example.culturle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Artwork : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artwork)
+    }
+}
