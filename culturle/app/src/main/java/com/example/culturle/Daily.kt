616f731d5f20b4d76631b058e517b6df8e3e63c8
hayden knox <knoha805@student.otago.ac.nz>
2022-08-12 15:38:18 +1200

    added the dropbox

--- culturle/app/src/main/java/com/example/culturle/Daily.kt
+++ culturle/app/src/main/java/com/example/culturle/Daily.kt
@@ -3,10 +3,15 @@ package com.example.culturle
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.culturle.R
+import android.widget.Toast
+
+
 
 class Daily : AppCompatActivity() {
 
@@ -22,17 +27,41 @@ class Daily : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily)
-        flag = true
-        b1 = findViewById<View>(R.id.guessButton) as Button
-        iv = findViewById<View>(R.id.hintImage) as ImageView?
-        b1!!.setOnClickListener {
-            i++
-            if (i == 3) i = 0
-            iv!!.setImageResource(images[i])
-            guesses++
-            if(guesses==6){
-                val intent = Intent(this, EndScreen::class.java)
-                startActivity(intent)
+        val autotextView = findViewById<AutoCompleteTextView>(R.id.autoTextView)
+        // Get the array of languages
+        val languages = resources.getStringArray(R.array.Languages)
+        // Create adapter and add in AutoCompleteTextView
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, languages
+        )
+        autotextView.setAdapter(adapter)
+
+        val button = findViewById<Button>(R.id.btn)
+        if (button != null) {
+            button?.setOnClickListener(View.OnClickListener {
+                val enteredText = getString(R.string.submitted_lang) + " " + autotextView.getText()
+            })
+
+            val autotextView = findViewById<AutoCompleteTextView>(R.id.autoTextView)
+            val languages = resources.getStringArray(R.array.Languages)
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1, languages
+            )
+            autotextView.setAdapter(adapter)
+            flag = true
+            b1 = findViewById<View>(R.id.guessButton) as Button
+            iv = findViewById<View>(R.id.hintImage) as ImageView?
+            b1!!.setOnClickListener {
+                i++
+                if (i == 3) i = 0
+                iv!!.setImageResource(images[i])
+                guesses++
+                if (guesses == 6) {
+                    val intent = Intent(this, EndScreen::class.java)
+                    startActivity(intent)
+                }
             }
         }
     }
