4dc315d3240d6de77c86a931415bc28283095739
Michael Young <michael.c.young00@gmail.com>
2022-08-12 15:28:25 +1200

    CI Test

--- culturle/app/src/test/java/com/example/culturle/ExampleUnitTest.kt
+++ culturle/app/src/test/java/com/example/culturle/ExampleUnitTest.kt
@@ -6,12 +6,12 @@ import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
- * TEST CI TEST
+ *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(4, 2 - 2)
     }
 }
