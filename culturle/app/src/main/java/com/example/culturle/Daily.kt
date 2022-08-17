59537c79fdb0e3850fe820ca12acc8acff22778b
Michael Young <michael.c.young00@gmail.com>
2022-08-17 16:27:31 +1200

    Added Basic WinScreen

--- culturle/app/src/main/java/com/example/culturle/Daily.kt
+++ culturle/app/src/main/java/com/example/culturle/Daily.kt
@@ -20,44 +20,53 @@ class Daily : AppCompatActivity() {
     private var flag = false
     private val images = intArrayOf(R.drawable.ic1, R.drawable.ic2, R.drawable.ic3)
     private var i = 0
-    public var guesses = 0;
+    private var enteredText = "NONE"
+    var guesses = 0;
+    var todaysCountry = "NONE"
     private lateinit var binding: Daily
-    //CI test
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daily)
         val autotextView = findViewById<AutoCompleteTextView>(R.id.autoTextView)
-        // Get the array of languages
-        val languages = resources.getStringArray(R.array.Languages)
+        // Get the array of countries
+        val countries = resources.getStringArray(R.array.Countries)
         // Create adapter and add in AutoCompleteTextView
         val adapter = ArrayAdapter(
             this,
-            android.R.layout.simple_list_item_1, languages
+            android.R.layout.simple_list_item_1, countries
         )
         autotextView.setAdapter(adapter)
 
-        val button = findViewById<Button>(R.id.btn)
-        if (button != null) {
-            button?.setOnClickListener(View.OnClickListener {
-                val enteredText = getString(R.string.submitted_lang) + " " + autotextView.getText()
-            })
+        todaysCountry = "Germany"
 
+        val guessBtn = findViewById<Button>(R.id.guessButton)
+        if (guessBtn != null) {
+            guessBtn.setOnClickListener(View.OnClickListener {
+                enteredText = autotextView.getText().toString()
+                val enteredText = getString(R.string.submitted_country) + " " + autotextView.getText()
+            })
             val autotextView = findViewById<AutoCompleteTextView>(R.id.autoTextView)
-            val languages = resources.getStringArray(R.array.Languages)
+            val countries = resources.getStringArray(R.array.Countries)
             val adapter = ArrayAdapter(
                 this,
-                android.R.layout.simple_list_item_1, languages
+                android.R.layout.simple_list_item_1, countries
             )
             autotextView.setAdapter(adapter)
             flag = true
             b1 = findViewById<View>(R.id.guessButton) as Button
             iv = findViewById<View>(R.id.hintImage) as ImageView?
             b1!!.setOnClickListener {
+                enteredText = autotextView.getText().toString()
                 i++
                 if (i == 3) i = 0
                 iv!!.setImageResource(images[i])
                 guesses++
+                if(enteredText == todaysCountry) {
+                    val intent = Intent(this, WinScreen::class.java)
+                    startActivity(intent)
+                }
                 if (guesses == 6) {
                     val intent = Intent(this, EndScreen::class.java)
                     startActivity(intent)
