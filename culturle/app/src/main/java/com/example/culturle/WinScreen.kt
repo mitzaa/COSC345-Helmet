320a73b8f9784b9c39491606484cb44eeb96d213
Michael Young <michael.c.young00@gmail.com>
2022-08-19 14:30:49 +1200

    Documentation Test

--- culturle/app/src/main/java/com/example/culturle/WinScreen.kt
+++ culturle/app/src/main/java/com/example/culturle/WinScreen.kt
@@ -10,7 +10,7 @@ class WinScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_win_screen)
-//
+
         //Set button object to the practiceButton id
         val homeButtonObj: Button = findViewById<View>(R.id.winHome) as Button
         //Create button listen event to switch over to Practice class activity
